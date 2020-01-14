55344d5ed1637f53ab6958d1050a98a868b4b6d0
Joseph Downs <downs.joseph@gmail.com>
2020-01-14 01:07:17 -0600

    Added basic delivery drone behavior.

--- src/steamlocomotive/DeliveryDrone.java
+++ src/steamlocomotive/DeliveryDrone.java
@@ -2,14 +2,271 @@ package steamlocomotive;
 
 import battlecode.common.*;
 
+import java.util.List;
+
 public strictfp class DeliveryDrone extends Unit {
+    //TODO implement behavior where drones carry miners to and from unreachable soup (e.g. TwoForOneAndTwoForAll)
+    //TODO implement interactions with cows (drowning them, dropping them on enemies, or both)
+    //TODO make drones stay out of range of enemy net shooters and HQ
+
+    public enum DroneState{
+        //The drone looks for hapless victims
+        ROAMING,
+        //The drone hunts its prey after sighting it
+        CHASING,
+        //The drone gives its new friend a bath
+        DUNKING
+    }
+
+    private static class Transition {
+        public DeliveryDrone.DroneState target;
+        public boolean madeAction;
+
+        public Transition(DeliveryDrone.DroneState target, boolean madeAction) {
+            this.target = target;
+            this.madeAction = madeAction;
+        }
+    }
+
+    // The mode that the drone is currently in.
+    private DeliveryDrone.DroneState state;
+    // Pathfinder for going to a location;
+    private BugPathfinder pathfinder;
+    // The number of steps that have been taken while pathfinding.
+    private int pathfindSteps;
+    // Contains up to TRACKED_WATER_COUNT valid water representatives we can visit.
+    private MapLocation[] waterReps;
+
 
     public DeliveryDrone(int id) {
         super(id);
+        this.pathfinder = null;
+        this.pathfindSteps = 0;
+        this.waterReps = new MapLocation[Config.TRACKED_SOUP_COUNT];
+        this.state = DroneState.ROAMING;
     }
 
-    @Override
-    public void run(RobotController rc, int turn) {
 
+    public void run(RobotController rc, int turn) throws GameActionException {
+        // Update soup knowledge by scanning surroundings.
+        this.scanSurroundings(rc);
+
+        // Swap on current state.
+        boolean madeAction;
+        do {
+            Transition trans;
+            switch (this.state) {
+                case CHASING: trans = this.chasing(rc); break;
+                case DUNKING: trans = this.dunking(rc); break;
+                default:
+                case ROAMING: trans = this.roaming(rc); break;
+            }
+
+            // Reset transient miner state.
+            if (this.state != trans.target) {
+                this.pathfinder = null;
+                this.pathfindSteps = 0;
+            }
+
+            this.state = trans.target;
+            madeAction = trans.madeAction;
+        } while (!madeAction);
+
+        // Useful for debugging.
+        if (this.pathfinder != null) rc.setIndicatorDot(this.pathfinder.goal(), 255, 0, 0);
     }
+
+
+    public void scanSurroundings(RobotController rc) throws GameActionException {
+        // Water memory; track recently seen water.
+        Utils.traverseSensable(rc, loc -> {
+            boolean isFlooding = rc.senseFlooding(loc);
+            if (!isFlooding) return;
+
+            // Ignore water which is already close to a representative.
+            for (int i = 0; i < waterReps.length; i++) {
+                MapLocation rep = waterReps[i];
+                if (rep != null && loc.distanceSquaredTo(rep) <= Config.REPRESENTATIVE_THRESHOLD) {
+                    return;
+                }
+            }
+
+            // Try to designate as a representative. If there is a slot, fill it.
+            for (int i = 0; i < waterReps.length; i++) {
+                if(waterReps[i] == null) {
+                    waterReps[i] = loc;
+                    return;
+                }
+            }
+
+            //TODO remove representatives from waterReps if they are no longer flooded
+
+            // Otherwise replace randomly.
+            this.waterReps[this.rng.nextInt(Config.TRACKED_SOUP_COUNT)] = loc;
+        });
+    }
+
+
+
+    /** Implements roaming behavior, where the drone roams until it finds an enemy somewhere. */
+    //TODO allow drones to roam on water (though should maybe favor roaming on land to find targets?)
+    //Because drones are using BugPathfinder to roam, they don't realize they can roam over water. It's very sad.
+    public Transition roaming(RobotController rc) throws GameActionException {
+        boolean isWaterToGoTo = false;
+        for (MapLocation loc : waterReps) {
+            if (loc != null) {
+                isWaterToGoTo = true;
+                break;
+            }
+        }
+
+        //Check if carrying anything. If so, transition to dunking.
+        if (rc.isCurrentlyHoldingUnit() && isWaterToGoTo) {
+            return new Transition(DroneState.DUNKING, false);
+        }
+
+        //Look for enemy robot. If see one and not currently holding a unit, transition to chasing.
+        if (!rc.isCurrentlyHoldingUnit()) {
+            RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+            for (RobotInfo nearbyEnemy : enemyRobots) {
+                if (nearbyEnemy.type.canBePickedUp()) {
+                    return new Transition(DroneState.CHASING, false);
+                }
+            }
+        }
+
+        // If the pathfinder is inactive or finished, pick a new random location to pathfind to.
+        if (this.pathfinder == null || this.pathfinder.finished(rc.getLocation())) {
+            // TODO: More intelligent target selection. We choose randomly for now.
+            // Suggestion: Drone remembers where it last picked enemy up and goes back. If nobody there, roam randomly.
+
+            MapLocation target = new MapLocation(this.rng.nextInt(rc.getMapWidth()), this.rng.nextInt(rc.getMapHeight()));
+
+            this.pathfinder = this.newPathfinder(target, true);
+        }
+
+        // Obtain a movement from the pathfinder and follow it.
+        Direction move = this.pathfinder.findMove(rc.getLocation(), dir -> BugPathfinder.canMoveF(rc, dir));
+        if (move != null && move != Direction.CENTER) rc.move(move);
+        this.pathfindSteps++;
+
+        return new Transition(DroneState.ROAMING, true);
+    }
+
+
+    /** Travel behavior, where a drone travels to a known water location to dunk. */
+    public Transition dunking(RobotController rc) throws GameActionException {
+        //If not carrying anything, transition to roaming.
+        if (!rc.isCurrentlyHoldingUnit()) {
+            return new Transition(DroneState.ROAMING, false);
+        }
+
+        // If we can dunk an enemy, immediately do so and go back to roaming to find more victims.
+        for (Direction dir : Direction.allDirections()) {
+            if (rc.canDropUnit(dir) && rc.senseFlooding(rc.getLocation().add(dir))) {
+                rc.dropUnit(dir);
+                return new Transition(DroneState.ROAMING, true);
+            }
+        }
+
+        // If no pathfinder, create it to the closest water.
+        if (this.pathfinder == null) {
+            MapLocation closest = waterReps[0];
+            int bestDistance = waterReps[0] == null ? Integer.MAX_VALUE : closest.distanceSquaredTo(rc.getLocation());
+            for (int i = 1; i < waterReps.length; i++) {
+                if (waterReps[i] == null) continue;
+                int dist = waterReps[i].distanceSquaredTo(rc.getLocation());
+                if (dist >= bestDistance) continue;
+
+                closest = waterReps[i];
+                bestDistance = dist;
+            }
+
+            // If all water has been unflooded, cry a little and roam.
+            if (closest == null) {
+                return new Transition(DroneState.ROAMING, false);
+            } else {
+                this.pathfinder = this.newPathfinder(closest, true);
+            }
+        }
+
+
+
+        // Obtain a movement from the pathfinder and follow it.
+        Direction move = this.pathfinder.findMove(rc.getLocation(), dir -> BugPathfinder.canMoveF(rc, dir));
+        if (move != null && move != Direction.CENTER) rc.move(move);
+        this.pathfindSteps++;
+
+        return new Transition(DroneState.DUNKING, true);
+    }
+
+
+    public Transition chasing(RobotController rc) throws GameActionException {
+        boolean isWaterToGoTo = false;
+        for (MapLocation loc : waterReps) {
+            if (loc != null) {
+                isWaterToGoTo = true;
+                break;
+            }
+        }
+        MapLocation droneLoc = rc.getLocation();
+
+        //Check if carrying anything. If so, transition to dunking.
+        if (rc.isCurrentlyHoldingUnit() && isWaterToGoTo) {
+            return new Transition(DroneState.DUNKING, false);
+        }
+
+        //Look for enemy robot. If see one, identify closest unit that can be picked up and move towards it.
+        //If can already pick up unit, do so and transition to dunking.
+        //First, identify map location of closest enemy unit that can be picked up
+        MapLocation closestEnemyLoc = droneLoc;
+        int closestEnemyDist = 500;
+        if (!rc.isCurrentlyHoldingUnit()) {
+            RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+            for (RobotInfo nearbyEnemy : enemyRobots) {
+                if (nearbyEnemy.type.canBePickedUp()) {
+                    if (rc.canPickUpUnit(nearbyEnemy.ID)) {
+                        rc.pickUpUnit(nearbyEnemy.ID);
+                        return new Transition(DroneState.DUNKING, true);
+                    }
+                    int enemyDist = droneLoc.distanceSquaredTo(nearbyEnemy.location);
+                    if (enemyDist < closestEnemyDist) {
+                        closestEnemyDist = enemyDist;
+                        closestEnemyLoc = nearbyEnemy.location;
+                    }
+                }
+            }
+        }
+
+        //This if statement shouldn't trigger.
+        //If something goes wrong finding a nearby enemy's location, this should trigger and send the drone north.
+        if (closestEnemyDist == 500) {
+            closestEnemyLoc = droneLoc.add(Direction.NORTH);
+        }
+
+        //Todo implement better chasing movement
+        
+        //Drone tries to move directly towards target. If it can't, moves the first direction it can.
+        Direction straightToClosest = droneLoc.directionTo(closestEnemyLoc);
+        if (rc.canMove(straightToClosest)) {
+            rc.move(straightToClosest);
+            return new Transition(DroneState.CHASING, true);
+        } else {
+            for (Direction adj : Direction.allDirections()) {
+                if (adj == Direction.CENTER) continue;
+                if (rc.canMove(adj)) {
+                    rc.move(adj);
+                    return new Transition(DroneState.CHASING, true);
+                }
+            }
+        }
+
+        //If we get to here, something is wrong. Go to roaming and hope things work out.
+        return new Transition(DroneState.ROAMING,false);
+    }
+
+
+
+
+
 }
--- src/steamlocomotive/FulfillmentCenter.java
+++ src/steamlocomotive/FulfillmentCenter.java
@@ -8,8 +8,28 @@ public class FulfillmentCenter extends Unit {
         super(id);
     }
 
-    @Override
-    public void run(RobotController rc, int turn) {
+    int numEarlyDronesBuilt = 0;
 
+    @Override
+    public void run(RobotController rc, int turn) throws GameActionException {
+        //Fulfillment center builds drones early, but not a lot
+        //Also builds lots of drones in late game
+        if (rc.getRoundNum() < 100 && numEarlyDronesBuilt <= 2) {
+            for (Direction adj : Direction.allDirections()) {
+                if (adj == Direction.CENTER) continue;
+                if (rc.canBuildRobot(RobotType.DELIVERY_DRONE, adj)) {
+                    rc.buildRobot(RobotType.DELIVERY_DRONE, adj);
+                    numEarlyDronesBuilt++;
+                }
+            }
+        }
+        else if (rc.getRoundNum() > 200) {
+            for (Direction adj : Direction.allDirections()) {
+                if (adj == Direction.CENTER) continue;
+                if (rc.canBuildRobot(RobotType.DELIVERY_DRONE, adj)) {
+                    rc.buildRobot(RobotType.DELIVERY_DRONE, adj);
+                }
+            }
+        }
     }
 }
