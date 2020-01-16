9e9ec902224b404b51e9b2a4815250455681db9d
Niels <nielsskornerup@gmail.com>
2020-01-16 16:34:27 -0600

    added landscaper wall location coms

--- src/steamlocomotive/Bitconnect.java
+++ src/steamlocomotive/Bitconnect.java
@@ -1,5 +1,6 @@
 package steamlocomotive;
 
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
 public class Bitconnect {
@@ -17,7 +18,11 @@ public class Bitconnect {
      * get a bit at an index of an integer
      */
     private boolean getBit(int integer, int index) {
-        return (integer>>index) %2 == 1;
+        return (integer >> index) % 2 == 1;
+    }
+
+    private int setBit(int integer, int index, boolean value) {
+        return value ? integer | (1 << index) : integer & ~(1 << index);
     }
 
     /**
@@ -28,9 +33,27 @@ public class Bitconnect {
     }
 
     /**
-     * create a checksum for a given message
+     * create a checksum for a given message of 6 ints
      */
     private int makeChecksum(int[] message) {
         return 0;
+    }
+
+    /**
+     * extract a checksum from a 7 int message
+     */
+    private int getChecksum(int[] message) {
+        return 0;
+    }
+
+    public MapLocation[] getLandscaperLocations(RobotController rc) {
+        return null;
+    }
+
+    /**
+     * assumed that map locations are a subset of a 3x3 grid with the center as center
+\    */
+    public void sendLandscaperLocations(RobotController rc, MapLocation center, MapLocation[] mapLocations) {
+
     }
 }
