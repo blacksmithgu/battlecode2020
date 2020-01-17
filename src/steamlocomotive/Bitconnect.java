a9420b483b5d04f6db9721f967ad2e50a5dd261c
Niels <nielsskornerup@gmail.com>
2020-01-16 20:12:58 -0600

    minor fixed to bitconnect

--- src/steamlocomotive/Bitconnect.java
+++ src/steamlocomotive/Bitconnect.java
@@ -192,13 +192,14 @@ public class Bitconnect {
     public Bitconnect(RobotController rc, int width, int height) throws GameActionException {
         this.width = width;
         this.height = height;
-        for(int turn = 0; turn < 20; turn++) {
+        for(int turn = 1; turn < 20 && turn < rc.getRoundNum(); turn++) {
             Transaction[] transactions = rc.getBlock(turn);
             for(Transaction transaction: transactions) {
                 Block block = Block.extractBlock(transaction.getMessage());
                 if(block != null) {
                     HQSurroundings surroundings = HQSurroundings.fromMessage(block);
                     if(surroundings!=null) {
+                        System.out.println("Our HQ is at: " + surroundings.hq);
                         this.ourHQSurroundings = surroundings;
                     }
                 }
@@ -207,12 +208,16 @@ public class Bitconnect {
     }
 
     public void updateForTurn(RobotController rc) throws GameActionException {
+        if(rc.getRoundNum() == 1) {
+            return;
+        }
         Transaction[] transactions = rc.getBlock(rc.getRoundNum()-1);
         for(Transaction transaction: transactions) {
             Block block = Block.extractBlock(transaction.getMessage());
             if(block != null) {
                 HQSurroundings surroundings = HQSurroundings.fromMessage(block);
                 if(surroundings!=null) {
+                    System.out.println("Our HQ is at: " + surroundings.hq);
                     this.ourHQSurroundings = surroundings;
                 }
             }
