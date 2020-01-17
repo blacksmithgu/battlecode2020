d9480cf56962d5378180198b3a263a49dd3b6382
Niels <nielsskornerup@gmail.com>
2020-01-16 19:43:36 -0600

    added a test for hq location message

--- test/steamlocomotive/BitconnectTest.java
+++ test/steamlocomotive/BitconnectTest.java
@@ -1,7 +1,14 @@
 package steamlocomotive;
 
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.Transaction;
 import org.junit.Test;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
 
 public class BitconnectTest {
 
@@ -13,7 +20,15 @@ public class BitconnectTest {
     }
 
     @Test
-    public void TestHQWallCode() {
-
+    public void TestHQWallCode() throws GameActionException {
+        RobotController rc = Mockito.mock(RobotController.class);
+        Mockito.when(rc.getBlock(anyInt())).thenReturn(new Transaction[0]);
+        Mockito.when(rc.getTeamSoup()).thenReturn(Integer.MAX_VALUE);
+        Bitconnect bitconnect = new Bitconnect(rc, 10, 10);
+        Bitconnect.HQSurroundings surroundings = new Bitconnect.HQSurroundings(new MapLocation(1,2), new MapLocation[0]);
+        Bitconnect.Block block = bitconnect.sendLandscaperLocations(rc, surroundings);
+        Bitconnect.HQSurroundings result = Bitconnect.HQSurroundings.fromMessage(block);
+        System.out.println(result);
+        assertTrue(surroundings.equals(result));
     }
 }
