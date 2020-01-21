09bc708014ca2a423a4818dc1b37680e0001911b
Joseph Downs <downs.joseph@gmail.com>
2020-01-20 21:16:51 -0600

    Builds landscapers based on size of walls.

--- src/steamlocomotive/DesignSchool.java
+++ src/steamlocomotive/DesignSchool.java
@@ -50,15 +50,16 @@ public class DesignSchool extends Unit {
         // The design school near the HQ builds 8 landscapers quickly, so that the wall gets up as fast as possible
         // Only does this building every other turn so that the first design school can get out its early drones
         // Added +50 so this doesn't kill our early econ and prevent any drone building
-        if (isNearHQ && numLandscapersBuilt < 8 && rc.getTeamSoup() >= RobotType.REFINERY.cost + 20) {
+        if (isNearHQ && numLandscapersBuilt < wallLocations.adjacentWallSpots.length && rc.getTeamSoup() >= RobotType.REFINERY.cost + 20) {
             buildLandscaperBasic(rc);
             numLandscapersBuilt++;
             return;
         }
 
         // If the HQ has dirt on it and friendly landscapers don't outnumber enemy landscapers, build landscaper
+        // The plus 10 is so that emergency drones are more likely to be built than emergency landscapers
         if (isNearHQ && rc.canSenseLocation(friendlyHQLoc)) {
-            if (rc.getTeamSoup() >= RobotType.LANDSCAPER.cost && hqNeedsHelp(rc, friendlyHQLoc, nearbyRobots, schoolTeam)) {
+            if (rc.getTeamSoup() >= RobotType.LANDSCAPER.cost + 10 && hqNeedsHelp(rc, friendlyHQLoc, nearbyRobots, schoolTeam)) {
                     buildLandscaperBasic(rc);
                     numLandscapersBuilt++;
                     return;
