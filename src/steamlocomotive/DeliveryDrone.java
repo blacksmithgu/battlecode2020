c2864c41d54aa3918e13c3814f4e66025351affc
Joseph Downs <downs.joseph@gmail.com>
2020-01-26 16:38:22 -0600

    Drone run from net guns.

--- src/steamlocomotive/DeliveryDrone.java
+++ src/steamlocomotive/DeliveryDrone.java
@@ -317,6 +317,13 @@ public strictfp class DeliveryDrone extends Unit {
                 }
             }
         });
+
+        if (closestEnemyNetGun != null && rc.getLocation().distanceSquaredTo(closestEnemyNetGun) < GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED) {
+            moveAway(rc, closestEnemyNetGun);
+        }
+        else if (comms.enemyHq() != null && rc.getLocation().distanceSquaredTo(comms.enemyHq()) < GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED) {
+            moveAway(rc, comms.enemyHq());
+        }
     }
 
     /**
@@ -983,5 +990,12 @@ public strictfp class DeliveryDrone extends Unit {
         if (enemyHQLoc != null && enemyHQLoc.distanceSquaredTo(target) <= GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED) return false;
 
         return true;
+    }
+
+    private void moveAway(RobotController rc, MapLocation loc) throws GameActionException{
+        Direction directlyAway = loc.directionTo(rc.getLocation());
+        if (rc.canMove(directlyAway)) rc.move(directlyAway);
+        else if (rc.canMove(directlyAway.rotateRight())) rc.move(directlyAway.rotateRight());
+        else if (rc.canMove(directlyAway.rotateLeft())) rc.move(directlyAway.rotateLeft());
     }
 }
